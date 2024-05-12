d68eb6883aa15975351e982e2ce41f4cecb5d936
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-13 03:14:00 +0530

    chore: update layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,9 +22,21 @@
 
 <div class="relative flex h-screen flex-col">
 	<div class="grid flex-1 grid-cols-[280px_1fr]">
-		<aside>
-			<div class="flex flex-col gap-4 px-6 py-6 pr-6 lg:py-8">
-				<div>
+		<aside class="border-r">
+			<div class="flex flex-col px-6 py-6 pr-6 lg:py-8">
+				<div class="pb-4">
+					<a
+						href="/"
+						class={clsx(
+							'w-fit text-xl font-medium hover:underline',
+							$page.url.pathname === '/' ? 'text-blue-500 underline opacity-100' : 'opacity-65'
+						)}
+					>
+						WebGPU Examples
+					</a>
+				</div>
+
+				<div class="pb-2">
 					<input
 						oninput={(event) => {
 							search = event.currentTarget.value;
@@ -34,35 +46,23 @@
 						placeholder="Search"
 					/>
 				</div>
-				<div class="flex flex-col gap-4">
-					<div>
-						<a
-							href="/"
-							class={clsx('w-fit hover:underline', $page.url.pathname === '/' ? 'opacity-100' : 'opacity-65')}
-						>
-							About
-						</a>
-					</div>
 
-					<div class="flex flex-col gap-4">
-						{#each filteredExamples as { group, examples }}
-							<div>
-								<span class="text-xl capitalize text-gray-400">{group}</span>
-								<div class="flex flex-col">
-									{#each examples as { href, title }}
-										<a
-											{href}
-											class={clsx(
-												'w-fit capitalize hover:underline',
-												$page.url.pathname === href ? 'text-black underline' : 'text-gray-600'
-											)}>{title}</a
-										>
-									{/each}
-								</div>
-							</div>
-						{/each}
+				{#each filteredExamples as { group, examples }}
+					<div class="pb-2">
+						<span class="text-xl capitalize text-gray-400">{group}</span>
+						<div class="flex flex-col py-1">
+							{#each examples as { href, title }}
+								<a
+									{href}
+									class={clsx(
+										'w-fit capitalize hover:underline',
+										$page.url.pathname === href ? 'text-blue-500 underline' : 'text-gray-500'
+									)}>{title}</a
+								>
+							{/each}
+						</div>
 					</div>
-				</div>
+				{/each}
 			</div>
 		</aside>
 		<main class="relative">
