757cfd11c6478e1b6d8f4b5d5cfb6ecf62088163
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-14 00:20:38 +0530

    update: webgpu examples routing

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,6 @@
 import { groupBy } from '@/lib/utils';
 
-const modules = import.meta.glob('$lib/webgpu/examples/**/*.ts');
+const modules = Object.keys(import.meta.glob('$lib/webgpu/examples/**/*.ts'));
 
 type Example = {
 	group?: string;
@@ -9,14 +9,18 @@ type Example = {
 };
 
 export const load = async () => {
-	const examples = Object.keys(modules).map((path) => {
+	const examples = modules.map((path) => {
 		let [group, title] = path.split('/').slice(-2);
 
+		// remove the file extension
 		title = title.replace('.ts', '');
+
+		// remove the leading number and dash
+		group = group.replace(/(\d+)-/, '');
 
 		const href = `/${group}/${title}`;
 
-		group = group.replace(/(\d+)-/, '');
+		// replace dashes with spaces
 		group = group.replace(/-/g, ' ');
 
 		return { group, title, href } satisfies Example;
--- src/routes/[group]/[example]/+page.ts
+++ src/routes/[group]/[example]/+page.ts
@@ -1,17 +1,20 @@
-const modules = import.meta.glob('$lib/webgpu/examples/**/*.ts');
+const modules = Object.keys(import.meta.glob('$lib/webgpu/examples/**/*.ts'));
 
 export const load = async ({ params }) => {
 	const { group, example } = params;
 
-	return { group, example };
+	const [modulePath] = modules.filter((path) => path.includes(group));
+	const [folder] = modulePath.split('/').slice(-2);
+
+	return { group: folder, example };
 };
 
 export const entries = () => {
-	const paths = Object.keys(modules).map((path) => {
-		// eslint-disable-next-line prefer-const
+	const paths = modules.map((path) => {
 		let [group, example] = path.split('/').slice(-2);
 
 		example = example.replace('.ts', '');
+		group = group.replace(/(\d+)-/, '');
 
 		return { group, example };
 	});
