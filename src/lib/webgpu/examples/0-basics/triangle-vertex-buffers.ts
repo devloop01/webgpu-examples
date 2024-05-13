e4fb7e43bf2d735b4a0c1c7f26a477c2d6a07831
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-14 01:37:31 +0530

    feat(example): add `Triangle Vertext Buffers`

---
+++ src/lib/webgpu/examples/0-basics/triangle-vertex-buffers.ts
@@ -0,0 +1,95 @@
+import type { WebGPU } from '$lib/webgpu';
+
+export default function (wgpu: WebGPU) {
+	const module = wgpu.device.createShaderModule({
+		label: 'triangle shader',
+		code: shader
+	});
+
+	const pipeline = wgpu.device.createRenderPipeline({
+		label: 'triangle pipeline',
+		layout: 'auto',
+		vertex: {
+			module,
+			entryPoint: 'vsMain',
+			buffers: [
+				{
+					arrayStride: 2 * 4, // 2 floats * 4 bytes
+					attributes: [
+						{ shaderLocation: 0, offset: 0, format: 'float32x2' } // position
+					]
+				}
+			]
+		},
+		fragment: {
+			module,
+			entryPoint: 'fsMain',
+			targets: [{ format: wgpu.presentationFormat }]
+		},
+		primitive: {
+			topology: 'triangle-list'
+		}
+	});
+
+	// prettier-ignore
+	const vertexData = new Float32Array([
+		// positions
+		0.2,  0.5, 
+		0.5, -0.6, 
+	   -0.5, -0.2
+   ]);
+
+	const vertexBuffer = wgpu.device.createBuffer({
+		label: 'triangle vertex buffer',
+		size: vertexData.byteLength,
+		usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+	});
+
+	wgpu.device.queue.writeBuffer(vertexBuffer, 0, vertexData);
+
+	return () => {
+		const encoder = wgpu.device.createCommandEncoder();
+
+		const pass = encoder.beginRenderPass({
+			colorAttachments: [
+				{
+					view: wgpu.context.getCurrentTexture().createView(),
+					loadOp: 'clear',
+					storeOp: 'store',
+					clearValue: [0.95, 0.95, 0.95, 1.0]
+				}
+			]
+		});
+
+		pass.setPipeline(pipeline);
+		pass.setVertexBuffer(0, vertexBuffer);
+		pass.draw(3);
+
+		pass.end();
+
+		wgpu.device.queue.submit([encoder.finish()]);
+	};
+}
+
+const shader = /*wgsl*/ `
+	struct VertexInput {
+		@location(0) position : vec2f,
+	}
+
+
+	struct VertexOutput {
+		@builtin(position) position : vec4f
+	}
+
+	@vertex
+	fn vsMain(vsInput : VertexInput) -> VertexOutput {
+		var output : VertexOutput;
+		output.position = vec4f(vsInput.position, 0.0, 1.0);
+		return output;
+	}
+
+	@fragment
+	fn fsMain(fsInput : VertexOutput) -> @location(0) vec4f {
+		return vec4f(0.2, 0.8, 0.2, 1.0);
+	}
+`;
